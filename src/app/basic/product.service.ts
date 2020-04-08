41083e6d271b720880d19f977b18c80b33f94791
Santosh Marigowda <santosharakere@gmail.com>
2020-04-08 14:45:18 +0100

    injecting HTTPClient into ProductService and using it

--- src/app/basic/product.service.ts
+++ src/app/basic/product.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 export class Product {
   constructor(
@@ -9,14 +10,23 @@ export class Product {
   ){}
 }
 
-// @Injectable({
-//   providedIn: 'root'
-// })
+@Injectable({
+  providedIn: 'root'
+})
 export class ProductService {
-
-  constructor() { }
+  data;
+  constructor(private http: HttpClient) {
+    this.data = http.get('assets/product.json').subscribe(data => {
+      console.log(data);
+      this.data = data;
+    });
+    console.log(this.data);
+  }
 
   getProduct(): Product {
+    // If you want to use the data returned by the http request
+    // then return an Observable
+    // so that the client can subscribe to the observable to get the data
     return new Product(0, 'iPhone 7', 249.99, 'The latest iPhone, 7 inch screen');
   }
 }
